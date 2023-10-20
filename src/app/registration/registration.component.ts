8c97b5f176971711d4761c01d4c6cf7286032c43
Rishika Jain <rishika.10726010@ltimindtree.com>
2023-10-20 16:22:28 +0530

    we are live 11

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -8,7 +8,7 @@ import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
-  title = 'Angular Form Validation';
+  title = 'Please';
   angForm!: FormGroup;
   constructor(private fb: FormBuilder) {
    this.createForm();
